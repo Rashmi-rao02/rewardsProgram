5110886d92f282c61d9556a33d50fc21c9ea0915
Rashmi-rao02 <k.rashmirao02@gmail.com>
2026-02-05 15:44:27 -0500

    Adding RewardSummary and update rewards logic

--- src/main/java/com/retailer/reward/dto/RewardSummaryResponse.java
+++ src/main/java/com/retailer/reward/dto/RewardSummaryResponse.java
@@ -1,4 +1,17 @@
 package com.retailer.reward.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
 public class RewardSummaryResponse {
+    private List<RewardResponse> customerRewards;
+    private int grandTotalPoints;
+    private LocalDate reportStartDate;
+    private LocalDate reportEndDate;
 }
